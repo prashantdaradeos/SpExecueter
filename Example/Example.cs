a31f784f97084c9432fdf1ef69f7e373b3092a7e
prashantdaradeos <prashantdaradeos@gmail.com>
2025-07-12 16:36:20 +0530

    Testing Completed

--- Example/Example.cs
+++ Example/Example.cs
@@ -11,87 +11,65 @@ namespace Example.DataAccess;
 [SpHandler(Lifetime.Scoped)]
 public interface IScopedSpExecutor
 {
-    [StoredProcedure("Test_GetSingleRecord")]
-    ValueTask<Record4> GetSingleRecordAsync(string connectionString, HeaderParameters parameters);
+    [StoredProcedure("SaveFullHeaderDetails")]
+    ValueTask<HeaderResult> GetSingleRecordAsync(string connectionString, HeaderParameters parameters);
 
-    [StoredProcedure("Test_GetTupleOfRecords")]
-    ValueTask<(Record4, Record5)> GetTupleOfRecordsAsync(string connectionString, HeaderParameters parameters);
+    [StoredProcedure("SaveFullHeaderDetails")]
+    ValueTask<(HeaderResult, Record4Result, AuditInfoResult)> GetTupleOfRecordsAsync(string connectionString, HeaderParameters parameters);
 
-    [StoredProcedure("Test_GetListAndObject")]
-    ValueTask<(List<Record4>, Record5)> GetListAndObjectAsync(string connectionString, CompositeRecord composite);
+    [StoredProcedure("SaveFullHeaderDetails")]
+    ValueTask<(List<HeaderResult>, Record4Result)> GetListAndObjectAsync(string connectionString, HeaderParameters parameters);
 }
 
 [SpHandler(Lifetime.Singleton)]
 public interface ISingletonSpExecutor
 {
-    [StoredProcedure("Test_GetListByInput")]
-    ValueTask<List<Record5>> GetListByInputAsync(string connectionString, Record7 parameters);
+    [StoredProcedure("SaveFullHeaderDetails")]
+    ValueTask<List<HeaderResult>> GetListAsync(string connectionString, HeaderParameters parameters);
 
-    [StoredProcedure("Test_GetTwoLists")]
-    ValueTask<(List<CompositeRecord>, List<Record5>)> GetTwoListsAsync(string connectionString);
+    [StoredProcedure("SaveFullHeaderDetails")]
+    ValueTask<(List<HeaderResult>, List<Record4Result>)> GetTwoListsAsync(string connectionString, HeaderParameters parameters);
 
-    [StoredProcedure("Test_GetGenericResponse")]
+    [StoredProcedure("InsertHeaderStatic")]
     ValueTask<GenericSpResponse> GetGenericResponseAsync(string connectionString);
+    [StoredProcedure("InsertHeaderOnly")]
+    ValueTask<GenericSpResponse> GetGenericResponseAsync(string connectionString, HeaderInfo parameters);
 
-    [StoredProcedure("Test_GetThreeLists")]
-    ValueTask<(List<CompositeRecord>, List<Record5>, List<HeaderParameters>)> GetThreeListsAsync(string connectionString);
+    [StoredProcedure("SaveFullHeaderDetails")]
+    ValueTask<(List<HeaderResult>, List<Record4Result>, List<AuditInfoResult>)> GetThreeListsAsync(string connectionString, HeaderParameters parameters);
 }
 
 #endregion
 
 #region DTOs
-
-/// <summary>
-/// Parameters passed to the primary stored procedures.
-/// </summary>
-public class HeaderParameters : ISpResponse
+public class HeaderInfo
 {
     public string Name { get; set; }
     public int Count { get; set; }
     public bool IsActive { get; set; }
     public long LargeNumber { get; set; }
     public double Ratio { get; set; }
-
-    public List<Record4> Record4Items { get; set; } = new();
-    public List<AuditInfo> AuditItems { get; set; } = new();
-
     public byte[] BinaryData { get; set; }
+    public DateTime OccurredOn { get; set; }
+    public DateTimeOffset OccurredAt { get; set; }
+    public TimeSpan Duration { get; set; }
+}
+public class HeaderParameters : HeaderInfo
+{
+ 
+    public List<Record4TableType> Record4Items { get; set; } = new();
+    public List<AuditInfoTableType> AuditItems { get; set; } = new();
 }
 
-public class Record4 : ISpResponse
+public class Record4TableType 
 {
+
     public string Name { get; set; }
     public int Count { get; set; }
     public bool IsActive { get; set; }
     public long LargeNumber { get; set; }
 }
-
-public class Record5 : ISpResponse
-{
-    public string Name { get; set; }
-    public int Count { get; set; }
-    public bool IsActive { get; set; }
-}
-
-public class CompositeRecord : ISpResponse
-{
-    public string Name { get; set; }
-    public int Count { get; set; }
-    public bool IsActive { get; set; }
-
-    public List<Record4> Record4Items { get; set; } = new();
-    public List<Record5> Record5Items { get; set; } = new();
-    public List<Record7> Record7Items { get; set; } = new();
-}
-
-public class Record7 : ISpResponse
-{
-    public string Name { get; set; }
-    public int Count { get; set; }
-    public bool IsActive { get; set; }
-}
-
-public class AuditInfo
+public class AuditInfoTableType
 {
     public string Description { get; set; }
     public int Count { get; set; }
@@ -104,99 +82,44 @@ public class AuditInfo
     public TimeSpan Duration { get; set; }
 }
 
+
+public class HeaderResult :ISpResponse
+{
+    public int HeaderId { get; set; }
+
+    public string Name { get; set; }
+    public int Count { get; set; }
+    public bool IsActive { get; set; }
+    public long LargeNumber { get; set; }
+    public decimal Ratio { get; set; }
+    public byte[] BinaryData { get; set; }
+    public DateTime OccurredOn { get; set; }
+    public DateTimeOffset OccurredAt { get; set; }
+    public TimeSpan Duration { get; set; }
+}
+public class Record4Result : ISpResponse
+{
+    public int Record4Id { get; set; }
+    public int HeaderId { get; set; }
+    public string Name { get; set; }
+    public int Count { get; set; }
+    public bool IsActive { get; set; }
+    public long LargeNumber { get; set; }
+}
+public class AuditInfoResult : ISpResponse
+{
+    public int AuditInfoId { get; set; }
+    public int HeaderId { get; set; }
+    public string Description { get; set; }
+    public int Count { get; set; }
+    public bool IsActive { get; set; }
+    public long LargeNumber { get; set; }
+    public decimal Ratio { get; set; }
+    public byte[] BinaryData { get; set; }
+    public DateTime OccurredOn { get; set; }
+    public DateTimeOffset OccurredAt { get; set; }
+    public TimeSpan Duration { get; set; }
+}
+
 #endregion
 
-
-/*
-
-using SpExecuter.Utility;
-
-namespace Example;
-
-
- 
-[SpHandler(Lifetime.Scoped)]
-public interface IClass1
-{
-    [StoredProcedure("1stSp")]
-    ValueTask<Class4> ExecuteFrom3(string conString, Class3 class3Obj);
-    [StoredProcedure("Sp_ExecuteForTupleWithObjects")]
-    ValueTask<(Class4, Class5)> ExecuteForTupleWithObjects(string conString, Class3 class3Obj);
-    [StoredProcedure("Sp_ExecuteForTupleWithListAndObject")]
-    ValueTask<(List<Class4>, Class5)> ExecuteForTuplewitListNObject(string conString, Class6 class6Obj);
-
-}
-
-[SpHandler(Lifetime.Singleton)]
-public interface IClass2
-{
-    [StoredProcedure("Sp_ExecuteFrom7")]
-    ValueTask<List<Class5>> ExecuteFrom7(string conString, Class7 class7Obj);
-    [StoredProcedure("Sp_ExecuteToLists")]
-    ValueTask<(List<Class6>, List<Class5>)> ExecuteToLists(string conString);
-    [StoredProcedure("Sp_ExecuteToListsAnother")]
-    ValueTask<GenericSpResponse> ExecuteToListsAnother(string conString);
-    [StoredProcedure("Sp_ExecuteToLists3")]
-    ValueTask<(List<Class6>, List<Class5>,List<Class3>)> ExecuteToLists3(string conString);
-
-}
-public class Class3:ISpResponse
-{
-    public string Class3String { get; set; }
-    public int Class3Int { get; set; }
-    public bool Class3Bool { get; set; }
-    public long Class3long { get; set; }
-    public double Class3double { get; set; }
-
-    public List<Class4> Class4List { get; set; } = new List<Class4>();
-    public List<Class8> Class8List { get; set; } = new List<Class8>();
-    public byte[] Class3Binary { get; set; }
-}
-
-
-public class Class4 : ISpResponse
-{
-    public string Class4String { get; set; }
-    public int Class4Int { get; set; }
-    public bool Class4Bool { get; set; }
-    public long Class4long { get; set; }
-
-}
-public class Class5 : ISpResponse
-{
-    public string Class5String { get; set; }
-    public int Class5Int { get; set; }
-    public bool Class5Bool { get; set; }
-}
-public class Class6 : ISpResponse
-{
-    public string Class6String { get; set; }
-    public int Class6Int { get; set; }
-    public bool Class6Bool { get; set; }
-    public List<Class4> Class4List { get; set; } = new List<Class4>();
-    public List<Class5> Class5List { get; set; } = new List<Class5>();
-    public List<Class7> Class7List { get; set; } = new List<Class7>();
- 
-
-}
-public class Class7 : ISpResponse
-{
-    public string Class7String { get; set; }
-    public int Class7Int { get; set; }
-    public bool Class7Bool { get; set; }
-}
-public class Class8
-{
-    public string Class8String { get; set; }
-    public int Class8Int { get; set; }
-    public bool Class8Bool { get; set; }
-    public long Class8long { get; set; }
-    public double Class8double { get; set; }
-    public byte[] Class8Binary { get; set; }
-    public DateTime Class8DateTime { get; set; }
-    public DateTimeOffset Class8Date { get; set; }
-    public TimeSpan Class8TimeSpan { get; set; }
-
-
-}
-*/
--- Example/Program.cs
+++ Example/Program.cs
@@ -1,188 +1,114 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Example.DataAccess;
+﻿using Example.DataAccess;
 using Microsoft.Extensions.DependencyInjection;
 using SpExecuter.Utility;
-using System;
-using System.Collections.Generic;
-using System.Data;
 using System.Text;
-using System.Threading.Tasks;
 
 var services = new ServiceCollection();
-services.ConfigureSpExecuter();
+services.ConfigureSpExecuter();             // Mandatory Call this method from Your APP
 
-// Build provider and resolve executors
 var provider = services.BuildServiceProvider();
 var scopedExecutor = provider.GetRequiredService<IScopedSpExecutor>();
 var singletonExecutor = provider.GetRequiredService<ISingletonSpExecutor>();
 
-string connectionString = "Server=localhost\\SQLEXPRESS;Database=Test;Trusted_Connection=True; TrustServerCertificate=True; Integrated Security=True;";
 
-// Sample input object
-var inputHeader = new HeaderParameters
+string connectionString = "Server=localhost\\SQLEXPRESS;Database=Test;Trusted_Connection=True; TrustServerCertificate=True; Integrated Security=True;";
+var headerOnly = new HeaderInfo
 {
-    Name = "demo-header",
-    Count = 123,
+    Name = "OnlyHeader",
+    Count = 1,
+    IsActive = false,
+    LargeNumber = 98765,
+    Ratio = 99.99,
+    BinaryData = Encoding.UTF8.GetBytes("one-header"),
+    OccurredOn = DateTime.UtcNow,
+    OccurredAt = DateTimeOffset.UtcNow,
+    Duration = TimeSpan.FromMinutes(10)
+};
+var header = new HeaderParameters
+{
+    Name = "Sample Header",
+    Count = 10,
     IsActive = true,
-    LargeNumber = 999999,
-    Ratio = 12.34,
-    BinaryData = Encoding.UTF8.GetBytes("hello"),
-    Record4Items = new()
+    LargeNumber = 1234567890,
+    Ratio = 45.67,
+    BinaryData = Encoding.UTF8.GetBytes("HelloWorld"),
+    OccurredOn = new DateTime(2025, 7, 12, 10, 30, 0),
+    OccurredAt = new DateTimeOffset(2025, 7, 12, 10, 30, 0, TimeSpan.FromHours(5.5)),
+    Duration = new TimeSpan(2, 15, 30), // 2h 15m 30s
+
+    Record4Items = new List<Record4TableType>
     {
-        new Record4 { Name = "row-1", Count = 1, IsActive = false, LargeNumber = 100 },
-        new Record4 { Name = "row-2", Count = 2, IsActive = true, LargeNumber = 200 },
-    },
-    AuditItems = new()
-    {
-        new AuditInfo
+        new Record4TableType
         {
-            Description = "Sample Text",
-            Count = 42,
+            Name = "Record A",
+            Count = 1,
             IsActive = true,
-            LargeNumber = 1234567890L,
-            Ratio = 98.76,
-            BinaryData = new byte[] { 0x01, 0x02, 0x03 },
-            OccurredOn = DateTime.Now,
+            LargeNumber = 1000
+        },
+        new Record4TableType
+        {
+            Name = "Record B",
+            Count = 2,
+            IsActive = false,
+            LargeNumber = 2000
+        }
+    },
+
+    AuditItems = new List<AuditInfoTableType>
+    {
+        new AuditInfoTableType
+        {
+            Description = "Audit Entry 1",
+            Count = 5,
+            IsActive = true,
+            LargeNumber = 5000,
+            Ratio = 99.99,
+            BinaryData = new byte[] { 0x01, 0x02 },
+            OccurredOn = DateTime.Now.Date,
             OccurredAt = DateTimeOffset.Now,
-            Duration = new TimeSpan(2, 30, 0)
+            Duration = TimeSpan.FromMinutes(90)
+        },
+        new AuditInfoTableType
+        {
+            Description = "Audit Entry 2",
+            Count = 8,
+            IsActive = false,
+            LargeNumber = 8888,
+            Ratio = 75.25,
+            BinaryData = new byte[] { 0x03, 0x04 },
+            OccurredOn = DateTime.Today.AddDays(-1),
+            OccurredAt = DateTimeOffset.Now.AddDays(-1),
+            Duration = TimeSpan.FromHours(1)
         }
     }
 };
 
-var compositeInput = new CompositeRecord
-{
-    Name = "composite-test",
-    Count = 42,
-    IsActive = false
-};
 
-var filterRecord = new Record7
-{
-    Name = "filter-test",
-    Count = 77,
-    IsActive = true
-};
 
-// Scoped executor calls
-(Record4 rec4, Record5 rec5) =
-    await scopedExecutor.GetTupleOfRecordsAsync(connectionString, inputHeader);
 
-(List<Record4> listRec4, Record5 singleRec5) =
-    await scopedExecutor.GetListAndObjectAsync(connectionString, compositeInput);
+HeaderResult single = await scopedExecutor.GetSingleRecordAsync(connectionString, header);
 
-// Singleton executor calls
-List<Record5> filteredList =
-    await singletonExecutor.GetListByInputAsync(connectionString, filterRecord);
+(HeaderResult h, Record4Result r4, AuditInfoResult a) =
+    await scopedExecutor.GetTupleOfRecordsAsync(connectionString, header);
 
-(List<CompositeRecord> listComposite, List<Record5> listRec5) =
-    await singletonExecutor.GetTwoListsAsync(connectionString);
+(List<HeaderResult> headers, Record4Result singleRecord4) =
+    await scopedExecutor.GetListAndObjectAsync(connectionString, header);
 
-GenericSpResponse genericResponse =
-    await singletonExecutor.GetGenericResponseAsync(connectionString);
+List<HeaderResult> headerList = await singletonExecutor.GetListAsync(connectionString, header);
 
-(List<CompositeRecord> listComposite2, List<Record5> listRec5b, List<HeaderParameters> listHeaderParams) =
-    await singletonExecutor.GetThreeListsAsync(connectionString);
+(List<HeaderResult> headers2, List<Record4Result> record4s2) =
+    await singletonExecutor.GetTwoListsAsync(connectionString,header);
+
+GenericSpResponse staticInsertResult = await singletonExecutor.GetGenericResponseAsync(connectionString);
+
+
+GenericSpResponse insertHeaderResult =
+    await singletonExecutor.GetGenericResponseAsync(connectionString, headerOnly);
+
+(List<HeaderResult> hList, List<Record4Result> rList, List<AuditInfoResult> aList) =
+    await singletonExecutor.GetThreeListsAsync(connectionString,header);
+
+var resultInStringArray = await SpExecutor.ExecuteSpToStringArray("SaveFullHeaderDetails", connectionString, 1, header);
 
 Console.WriteLine("Test execution completed.");
 
-
-
-/*// See https://aka.ms/new-console-template for more information
-using Example;
-using Microsoft.Extensions.DependencyInjection;
-using SpExecuter.Utility;
-using System.Data;
-using System.Text;
-
-
-
-var services = new ServiceCollection();
-services.ConfigureSpExecuter();
-// 2) Resolve services from the built IServiceProvider
-var provider = services.BuildServiceProvider();
-var class1 = provider.GetRequiredService<IClass1>();
-var class2 = provider.GetRequiredService<IClass2>();
-var a=DBConstants.tVPsdelegates;
-string conString = "Server=localhost\\SQLEXPRESS;Database=Test;Trusted_Connection=True; TrustServerCertificate=True; Integrated Security=True;";
-
-
-
-
-var class3Obj = new Class3
-{
-    Class3String = "demo-C3",
-    Class3Int = 123,
-    Class3Bool = true,
-    Class3long = 999999,
-    Class3double = 12.34,
-    Class3Binary = Encoding.UTF8.GetBytes("hello"),
-
-    Class4List = new()
-    {
-        new Class4
-        {
-            Class4String = "row-1",
-            Class4Int    = 1,
-            Class4Bool   = false,
-            Class4long   = 100
-        },
-          new Class4
-        {
-            Class4String = "row-2",
-            Class4Int    = 2,
-            Class4Bool   = true,
-            Class4long   = 200
-        },
-    },
-
-    Class8List  = new ()
-    {new (){
-        Class8String = "Sample Text",
-        Class8Int = 42,
-        Class8Bool = true,
-        Class8long = 1234567890L,
-        Class8double = 98.76,
-        Class8Binary = new byte[] { 0x01, 0x02, 0x03 },
-        Class8DateTime = DateTime.Now,
-        Class8Date = DateTimeOffset.Now,
-        Class8TimeSpan = new TimeSpan(2, 30, 0) // 2 hours, 30 minutes
-    } }
-
-};
-
-// ----- b) For ExecuteForTuplewitListNObject -----------------
-var class6Obj = new Class6
-{
-    Class6String = "demo-C6",
-    Class6Int = 42,
-    Class6Bool = false
-};
-
-// ----- c) For ExecuteFrom7 ----------------------------------
-var class7Obj = new Class7
-{
-    Class7String = "demo-C7",
-    Class7Int = 77,
-    Class7Bool = true
-};
-
-(Class4 c4, Class5 c5) =
-       await class1.ExecuteForTupleWithObjects(conString, class3Obj);
-
-(List<Class4> listC4, Class5 singleC5) =
-    await class1.ExecuteForTuplewitListNObject(conString, class6Obj);
-
-// IClass2 methods
-List<Class5> listC5 =
-    await class2.ExecuteFrom7(conString, class7Obj);
-
-(List<Class6> listC6, List<Class5> listC5b) =
-    await class2.ExecuteToLists(conString);
-
-GenericSpResponse generic =
-    await class2.ExecuteToListsAnother(conString);
-
-(List<Class6> listC6b, List<Class5> listC5c, List<Class3> listC3) =
-    await class2.ExecuteToLists3(conString);
-
-Console.WriteLine("");*/
